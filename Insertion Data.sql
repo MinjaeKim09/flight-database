6704294b03f549edf9685c3f8fb29f1cebc4f21a
MinjaeKim09 <66180468+MinjaeKim09@users.noreply.github.com>
2025-11-30 16:43:32 -0500

    Update Insertion Data.sql with MD5 hashed passwords

--- Insertion Data.sql
+++ Insertion Data.sql
@@ -7,13 +7,13 @@ INSERT INTO Airport(code, city, country, type) VALUES
 INSERT INTO Customer(email, password, name, address_number, address_street, address_city, address_state,
                      phone_number, passport_number, passport_expiration, passport_country, date_of_birth)
 VALUES
-('edward@example.com','edward123','Edward Chen','5','Wall St.','New York','NY','+1-212-555-1000','PUSA10001','2032-06-30','USA','1995-04-12'),
-('aderline@example.com','ade123','Aderline Smith','88','Broadway','New York','NY','+1-917-555-2200','PUSA20002','2031-10-31','USA','1992-09-05'),
-('cass@example.com','cass123','Cassandra Diaz','5','Ocean Blvd','Miami','FL','+1-305-555-3300','PMEX30003','2033-01-15','Mexico','1990-02-20'),
+('edward@example.com','6676e7d0995ebd8dbd136869a9358d14','Edward Chen','5','Wall St.','New York','NY','+1-212-555-1000','PUSA10001','2032-06-30','USA','1995-04-12'),
+('aderline@example.com','fa6a6bd136dec26a1dd5e326b7e43254','Aderline Smith','88','Broadway','New York','NY','+1-917-555-2200','PUSA20002','2031-10-31','USA','1992-09-05'),
+('cass@example.com','15698ae039abb8092677ea9c9f9a3d73','Cassandra Diaz','5','Ocean Blvd','Miami','FL','+1-305-555-3300','PMEX30003','2033-01-15','Mexico','1990-02-20'),
 ('mk8400@nyu.edu', '098f6bcd4621d373cade4e832627b4f6', 'Minjae Kim', '606', '170 Tillary St', 'Brooklyn', 'NY', '9714708881', '696942067', '2030-09-01', 'US', '2003-09-01');
 
 INSERT INTO Airline_Staff(username, password, airline_name, first_name, last_name, email, date_of_birth)
-VALUES ('jb_staff1','password','Jet Blue','Jamie','Baker','staff1@jetblue.com','1988-07-07');
+VALUES ('jb_staff1','5f4dcc3b5aa765d61d8327deb882cf99','Jet Blue','Jamie','Baker','staff1@jetblue.com','1988-07-07');
 
 INSERT INTO Works(airline_name, username) VALUES ('Jet Blue','jb_staff1');
 INSERT INTO Airline_Staff_Number(username, number) VALUES ('jb_staff1','+1-718-555-0100');
